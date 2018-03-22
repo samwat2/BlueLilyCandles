da25b3aef6e26d90223164fa0a9603506ab70305
Sammy Watchman <swsw872@gmail.com>
2018-03-22 17:43:37 -0600

    fixed some layout complications and items creation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+	has_many :user_items
+	has_many :users, through: :user_items
+	has_many :ratings
+	has_one_attached :image, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   ## The :user role is added by default and shouldn't be included in this list.             ##
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
-  petergate(roles: [:root_admin], multiple: true)                                      ##
+   petergate(roles: [:root_admin], multiple: true)                                      ##
   ############################################################################################ 
  
+ has_many :user_items
+ has_many :items, through: :user_items
+ has_many :ratings
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
