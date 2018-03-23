17bdfef7923590034e6fac2072bf38b868fa38a9
Sammy Watchman <swsw872@gmail.com>
2018-03-23 12:18:13 -0600

    allowed admin to edit items and destroy-- also updated models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-	has_many :user_items
+	has_many :user_items, dependent: :destroy
 	has_many :users, through: :user_items
-	has_many :ratings
+	has_many :ratings, dependent: :destroy
 	has_one_attached :image, dependent: :destroy
 end
