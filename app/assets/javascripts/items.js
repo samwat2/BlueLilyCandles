52d90b518a8766597eb78b35c3cb210aa76702d3
Sammy Watchman <swsw872@gmail.com>
2018-04-02 19:36:31 -0600

    added flickity attributes to jq

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -2,7 +2,7 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-
+//items side bar
 $(document).on('turbolinks:load', function(){
   $(document).on('click', '.sidebarCollapse', function() {
     let info_json = JSON.parse($(this).closest('.item').attr('data-info'));
@@ -17,4 +17,17 @@ $(document).on('turbolinks:load', function(){
   $(document).on('click', '#close-sidebar', function(){
     $('#sidebar').removeClass('active').addClass('inactive');
   });
+});
+
+//flickity carousel
+$(document).on('turbolinks:load', function(){
+  $('.carousel').flickity({
+    cellAlign: 'center',
+    groupCells: 1,
+    initialIndex: 4,
+    freeScroll: true,
+    wrapAround: true,
+    freeScrollFriction: 0.15,
+    selectedAttraction: 0.01,
+  })
 });
